1f436e608d9b6138a5c6f80bd84fdca0a2fe7fba
ld <laurent.dorier@dpd.com>
2016-09-26 16:22:06 +0200

    Minor modifs

--- bvg-widgets.php
+++ bvg-widgets.php
@@ -9,6 +9,9 @@ License: GPL3
 License URI: https://www.gnu.org/licenses/gpl-3.0.txt
 */
 
+/* Include nuLiga constants and variables */
+include plugin_dir_path(__FILE__).'nuliga_constants.php';
+
 /* SiteOrigin Widgets */
 function add_bvg_widgets_collection($folders){
     $folders[] = plugin_dir_path(__FILE__).'widgets/';
@@ -37,5 +40,81 @@ function bvg_admin_theme_script(){
     wp_enqueue_script('my-admin-theme', get_template_directory_uri() . '/../bvg/wp-admin.js', array( 'jquery' ) );
 }
 add_action('admin_enqueue_scripts', 'bvg_admin_theme_script');
+
+
+
+
+/* Global functions */
+if( !function_exists( 'bvg_editor' ) ){
+    function bvg_editor($str = '', $id)
+    {
+        wp_editor($str, $id);
+    }
+}
+
+if( !function_exists( 'remove_columns' ) ){
+    function remove_columns( $html='', $heads_toRemove = array(), $cols_toRemove = array() ){
+
+        $html_return = '';
+
+        //$html = str_replace( '&', '&amp;', $html );
+        // $html = str_replace( 'th>', 'td>', $html);
+        // $html = str_replace( '<th', '<td', $html);
+        $html = mb_convert_encoding($html, 'HTML-ENTITIES', "UTF-8");
+
+        // a new dom object
+        $dom = new domDocument;
+
+        // load the html into the object
+        echo 'XXXXXXX: '.$html;
+        die();
+        $dom->loadHTML( $html );
+
+        // discard white space
+        $dom->preserveWhiteSpace = false;
+
+        // Get all rows
+        $trList = $dom->getElementsByTagName('tr');
+
+        $tr_head = 1;
+        foreach ($trList as $tr) {
+
+            // Header
+            if( $tr_head === 1 && !empty( $heads_toRemove ) ){
+                $tdList = $tr->getElementsByTagName('th');
+                foreach( $heads_toRemove as $column_nb ){
+                    $tr->removeChild( $tdList->item( $column_nb ) );
+                }
+                $tr_head++;
+            }else if( $tr_head > 1 && !empty( $cols_toRemove ) ){
+                $tdList = $tr->getElementsByTagName('td');
+                // Content rows
+                foreach( $cols_toRemove as $column_nb ){
+                    /*
+                    echo $column_nb.': ';
+                    $xml = $tr->ownerDocument->saveXML($tr);
+                    echo $xml;
+                    echo '<br />';
+                    */
+                    $tr->removeChild( $tdList->item( $column_nb ) );
+                }
+
+            }
+
+        }
+
+        $html = $dom->saveHTML();
+
+        $html_return = $html;
+
+        $html_return = str_replace( '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">' , '' , $html_return );
+        $html_return = str_replace( '<html><body>' , '' , $html_return );
+        $html_return = str_replace( '</body></html>' , '' , $html_return );
+
+        return $html_return;
+
+    }
+}
+
 
 ?>
--- nuliga_constants.php
+++ nuliga_constants.php
@@ -11,17 +11,32 @@ define( 'NULIGA_DOMAIN', 'http://hbv-badminton.liga.nu' );
 
 $NULIGA_TEAMS_TABLE_URL = array(
     'A1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=HBV+16%2F17&group=22191', // Senior 1
-    'A2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=HBV+16%2F17&group=22195', // Senior 2
-    'A3' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=HBV+16%2F17&group=22197', // Senior 3
-    'A4' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=HBV+16%2F17&group=22193', // Senior 4
-    'A5' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=HBV+16%2F17&group=22203', // Senior 5
+    'A2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+16%2F17&group=22195', // Senior 2
+    'A3' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+16%2F17&group=22197', // Senior 3
+    'A4' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+16%2F17&group=22193', // Senior 4
+    'A5' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+16%2F17&group=22203', // Senior 5
 
-    'J1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=HBV+16%2F17&group=22217', // Jugend 1
+    'J1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+16%2F17&group=22217', // Jugend 1
 
-    'S1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=HBV+16%2F17&group=22233', // Schüler 1
-    'S2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=HBV+16%2F17&group=22232', // Schüler 2
+    'S1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+16%2F17&group=22233', // Schüler 1
+    'S2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+16%2F17&group=22232', // Schüler 2
 
-    'U13_1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=HBV+16%2F17&group=22235', // U13
+    'U13_1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+16%2F17&group=22235', // U13
+);
+
+$NULIGA_TEAMS_CALENDAR_URL = array(
+    'A1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=159092&championship=HBV+16%2F17&group=22191', // Senior 1
+    'A2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=158462&championship=Frankfurt+16%2F17&group=22195', // Senior 2
+    'A3' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=159310&championship=Frankfurt+16%2F17&group=22197', // Senior 3
+    'A4' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=159357&championship=Frankfurt+16%2F17&group=22193', // Senior 4
+    'A5' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=159403&championship=Frankfurt+16%2F17&group=22203', // Senior 5
+
+    'J1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=158660&championship=Frankfurt+16%2F17&group=22217', // Jugend 1
+
+    'S1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=158736&championship=Frankfurt+16%2F17&group=22233', // Schüler 1
+    'S2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=158753&championship=Frankfurt+16%2F17&group=22232', // Schüler 2
+
+    'U13_1' => NULIGA_DOMAIN . 'cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=158768&championship=Frankfurt+16%2F17&group=22235', // U13
 );
 
 ?>
