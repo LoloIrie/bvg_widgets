60e980a9217e857d8597beb92493777fbe7a9cee
ld <laurent.dorier@dpd.com>
2016-10-04 13:18:14 +0200

    Minor bug fix

--- nuliga_constants.php
+++ nuliga_constants.php
@@ -13,7 +13,7 @@ $NULIGA_TEAMS_TABLE_URL = array(
     'A1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=HBV+16%2F17&group=22191', // Senior 1
     'A2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+16%2F17&group=22195', // Senior 2
     'A3' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+16%2F17&group=22197', // Senior 3
-    'A4' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+16%2F17&group=22193', // Senior 4
+    'A4' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+16%2F17&group=22196', // Senior 4
     'A5' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+16%2F17&group=22203', // Senior 5
 
     'J1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+16%2F17&group=22217', // Jugend 1
@@ -28,7 +28,7 @@ $NULIGA_TEAMS_CALENDAR_URL = array(
     'A1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=159092&championship=HBV+16%2F17&group=22191', // Senior 1
     'A2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=158462&championship=Frankfurt+16%2F17&group=22195', // Senior 2
     'A3' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=159310&championship=Frankfurt+16%2F17&group=22197', // Senior 3
-    'A4' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=159357&championship=Frankfurt+16%2F17&group=22193', // Senior 4
+    'A4' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=159357&championship=Frankfurt+16%2F17&group=22196', // Senior 4
     'A5' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=159403&championship=Frankfurt+16%2F17&group=22203', // Senior 5
 
     'J1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=158660&championship=Frankfurt+16%2F17&group=22217', // Jugend 1
@@ -36,7 +36,7 @@ $NULIGA_TEAMS_CALENDAR_URL = array(
     'S1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=158736&championship=Frankfurt+16%2F17&group=22233', // Schüler 1
     'S2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=158753&championship=Frankfurt+16%2F17&group=22232', // Schüler 2
 
-    'U13_1' => NULIGA_DOMAIN . 'cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=158768&championship=Frankfurt+16%2F17&group=22235', // U13
+    'U13_1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=158768&championship=Frankfurt+16%2F17&group=22235', // U13
 );
 
 ?>
