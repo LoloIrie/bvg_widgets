f0dfae24bd06e6478fb4f14956db9dcafeacfa28
ld <laurent.dorier@dpd.com>
2017-07-19 14:25:26 +0200

    2017/2018 URLs

--- nuliga_constants.php
+++ nuliga_constants.php
@@ -9,6 +9,7 @@
 
 define( 'NULIGA_DOMAIN', 'http://hbv-badminton.liga.nu' );
 
+/* 2016-2017 */
 $NULIGA_TEAMS_TABLE_URL = array(
     'A1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=HBV+16%2F17&group=22191', // Senior 1
     'A2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+16%2F17&group=22195', // Senior 2
@@ -23,7 +24,6 @@ $NULIGA_TEAMS_TABLE_URL = array(
 
     'U13_1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+16%2F17&group=22235', // U13
 );
-
 $NULIGA_TEAMS_CALENDAR_URL = array(
     'A1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=159092&championship=HBV+16%2F17&group=22191', // Senior 1
     'A2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=158462&championship=Frankfurt+16%2F17&group=22195', // Senior 2
@@ -37,6 +37,36 @@ $NULIGA_TEAMS_CALENDAR_URL = array(
     'S2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=158753&championship=Frankfurt+16%2F17&group=22232', // Schüler 2
 
     'U13_1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=158768&championship=Frankfurt+16%2F17&group=22235', // U13
+);
+
+
+/* 2017-2018 */
+$NULIGA_TEAMS_TABLE_URL = array(
+    'A1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=HBV+17%2F18&group=23311', // Senior 1
+    'A2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+17%2F18&group=23317', // Senior 2
+    'A3' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+17%2F18&group=23319', // Senior 3
+    'A4' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+17%2F18&group=23320', // Senior 4
+    'A5' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+17%2F18&group=23325', // Senior 5
+
+    'J1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+17%2F18&group=23328', // Jugend 1
+    'S1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+17%2F18&group=23411', // Jugend 2
+    'S2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+17%2F18&group=23413', // Jugend 3
+
+    'U13_1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+17%2F18&group=23415', // Schüler 1
+);
+
+$NULIGA_TEAMS_CALENDAR_URL = array(
+    'A1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=164910&championship=HBV+17%2F18&group=23311', // Senior 1
+    'A2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=165191&championship=Frankfurt+17%2F18&group=23317', // Senior 2
+    'A3' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=164408&championship=Frankfurt+17%2F18&group=23319', // Senior 3
+    'A4' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=164430&championship=Frankfurt+17%2F18&group=23320', // Senior 4
+    'A5' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=164448&championship=Frankfurt+17%2F18&group=23325', // Senior 5
+
+    'J1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=165881&championship=Frankfurt+17%2F18&group=23328', // Jugend 1
+    'S1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=165993&championship=Frankfurt+17%2F18&group=23411', // Jugend 2
+    'S2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=165994&championship=Frankfurt+17%2F18&group=23413', // Jugend 3
+
+    'U13_1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=165898&championship=Frankfurt+17%2F18&group=23415', // Schüler 1
 );
 
 ?>
