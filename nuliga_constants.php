53aa9a8808d16304c3c83cb06f0e5b8cb2e6009e
LoloIrie <lolo_irie@etalkers.org>
2018-08-19 16:46:38 +0200

    Saison 2018/2019

--- nuliga_constants.php
+++ nuliga_constants.php
@@ -54,7 +54,6 @@ $NULIGA_TEAMS_TABLE_URL = array(
 
     'U13_1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+17%2F18&group=23415', // Schüler 1
 );
-
 $NULIGA_TEAMS_CALENDAR_URL = array(
     'A1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=164910&championship=HBV+17%2F18&group=23311', // Senior 1
     'A2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=165191&championship=Frankfurt+17%2F18&group=23317', // Senior 2
@@ -67,6 +66,37 @@ $NULIGA_TEAMS_CALENDAR_URL = array(
     'S2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=165994&championship=Frankfurt+17%2F18&group=23413', // Jugend 3
 
     'U13_1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=165898&championship=Frankfurt+17%2F18&group=23415', // Schüler 1
+);
+
+
+/* 2018-2019 */
+$NULIGA_TEAMS_TABLE_URL = array(
+    'A1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=HBV+18%2F19&group=24711', // Senior 1
+    'A2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+18%2F19&group=24636', // Senior 2
+    'A3' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+18%2F19&group=24634', // Senior 3
+    'A4' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+18%2F19&group=24733', // Senior 4
+    //'A5' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+18%2F19&group=23325', // Senior 5
+
+    'J1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+18%2F19&group=24593', // Jugend 1
+    'S1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+18%2F19&group=24831', // Jugend 2
+    'S2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+18%2F19&group=24832', // Jugend 3
+
+    'U13_1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+18%2F19&group=24833', // Schüler 1
+    'U13_2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/groupPage?championship=Frankfurt+18%2F19&group=24837', // Schüler 2
+);
+$NULIGA_TEAMS_CALENDAR_URL = array(
+    'A1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=172512&championship=HBV+18%2F19&group=24711', // Senior 1
+    'A2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=172106&championship=Frankfurt+18%2F19&group=24636', // Senior 2
+    'A3' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=172081&championship=Frankfurt+18%2F19&group=24634', // Senior 3
+    'A4' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=172706&championship=Frankfurt+18%2F19&group=24733', // Senior 4
+    //'A5' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=164448&championship=Frankfurt+18%2F19&group=23325', // Senior 5
+
+    'J1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=173317&championship=Frankfurt+18%2F19&group=24593', // Jugend 1
+    'S1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=173345&championship=Frankfurt+18%2F19&group=24831', // Jugend 2
+    'S2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=173389&championship=Frankfurt+18%2F19&group=24832', // Jugend 3
+
+    'U13_1' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=173420&championship=Frankfurt+18%2F19&group=24833', // Schüler 1
+    'U13_2' => NULIGA_DOMAIN . '/cgi-bin/WebObjects/nuLigaBADDE.woa/wa/teamPortrait?teamtable=173454&championship=Frankfurt+18%2F19&group=24837', // Schüler 2
 );
 
 ?>
